499ecf4c9d74690c801afa6bd90db9c1e88c273d
alexchaoss <45114914+alexchaoss@users.noreply.github.com>
2019-05-07 12:05:27 -0400

    adding character information for WoW

---
+++ app/src/main/java/com/example/blizzardprofiles/WoWClassEnum.java
@@ -0,0 +1,31 @@
+package com.example.blizzardprofiles;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum WoWClassEnum {
+    WARRIOR( "Warrior"), PALADIN( "Paladin"), HUNTER( "Hunter"), ROGUE( "Rogue"),
+    PRIEST( "Priest"), DEATHKNIGHT( "Death Knight"), SHAMAN( "Shaman"),MAGE("Mage"),
+    WARLOCK( "Warlock"), MONK( "Monk"), DRUID( "Druid"), DEMONHUNTER( "Demon Hunter");
+
+    private String className;
+
+    private static final Map<Integer, WoWClassEnum> lookup = new HashMap<Integer, WoWClassEnum>();
+
+    WoWClassEnum(String className){
+        this.className = className;
+    }
+
+    static {
+        int ordinal = 0;
+        for (WoWClassEnum woWClassEnum : EnumSet.allOf(WoWClassEnum.class)) {
+            lookup.put(ordinal, woWClassEnum);
+            ordinal += 1;
+        }
+    }
+
+    public static WoWClassEnum fromOrdinal(int ordinal) {
+        return lookup.get(ordinal);
+    }
+}
---
+++ app/src/main/java/com/example/blizzardprofiles/WoWThumbnail.java
@@ -0,0 +1,39 @@
+package com.example.blizzardprofiles;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class WoWThumbnail {
+
+    private WOWCharacters characters;
+    private Context context;
+    private ArrayList<Drawable> thumbnails = new ArrayList<>();
+    private final String THUMNAIL_URL = "http://render-us.worldofwarcraft.com/character/";
+
+    public WoWThumbnail(WOWCharacters wowCharacters, Context context){
+        this.characters = wowCharacters;
+        this.context = context;
+    }
+
+    public ArrayList<Drawable> getImageFromURL(){
+        ArrayList<String> urls = characters.getUrlThumbnail();
+        for(int i = 0; i<urls.size();i++){
+            try{
+                InputStream is = (InputStream) new URL(THUMNAIL_URL + urls.get(i)).getContent();
+                Drawable drawable = Drawable.createFromStream(is, "src");
+                thumbnails.add(drawable);
+            }catch (Exception e){
+                Log.e("Error",e.toString());
+                Drawable drawable = ContextCompat.getDrawable(context, R.drawable.no_avatar);
+                thumbnails.add(drawable);
+            }
+        }
+        return  thumbnails;
+    }
+}
