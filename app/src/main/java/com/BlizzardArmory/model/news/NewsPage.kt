72589eda7270750d3b4b146c4c1d8652ea02ec35
alexchaoss <45114914+alexchaoss@users.noreply.github.com>
2020-10-17 10:26:33 -0400

    more files from news feature

---
+++ app/src/main/java/com/BlizzardArmory/model/news/NewsPage.kt
@@ -0,0 +1,10 @@
+package com.BlizzardArmory.model.news
+
+data class NewsPage(
+        val game: String,
+        val title: String,
+        val author: String,
+        val date: String,
+        val imageURL: String,
+        val body: String
+)
---
+++ app/src/main/java/com/BlizzardArmory/model/news/UserNews.kt
@@ -0,0 +1,11 @@
+package com.BlizzardArmory.model.news
+
+class UserNews {
+    var blizzNews: Boolean = true
+    var wowNews: Boolean = true
+    var d3News: Boolean = true
+    var sc2News: Boolean = true
+    var owNews: Boolean = true
+    var hsNews: Boolean = true
+    var hotsNews: Boolean = true
+}
